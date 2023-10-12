66ab4a6f4cf9c5bbf4f05fba2c3c1d7af749f79b
Inosensey <dingcong.bae@gmail.com>
2023-10-12 12:34:36 +0800

    Added a reusable component called loading. To show loading animation stuff when doing queries

--- components/HomeComponents/SignUp.tsx
+++ components/HomeComponents/SignUp.tsx
@@ -1,12 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, CSSProperties } from "react";
 import { motion } from "framer-motion";
 import { useMutation } from "react-query";
 import axios, { Axios } from "axios";
+import HashLoader from "react-spinners/HashLoader";
 
 // Components
 import FirstStep from "./SignUpSteps/FirstStep";
 import SecondStep from "./SignUpSteps/SecondStep";
 import ThirdStep from "./SignUpSteps/ThirdStep";
+import Loading from "@/components/ReusableComponents/Loading/Loading";
+
+// Styles
+import styles from "@/css/HomeComponent/SignUp.module.css";
+
+// React Spinner Css Override
 
 // Set prop types
 interface props {
@@ -68,79 +75,92 @@ const SignUp = ({ setCurrentForm }: props) => {
   );
 
   return (
-    <section className="bg-white rounded-2xl text-black p-3 relative phone:w-11/12">
-      <form>
-        <div className={`${currentStep === 1 ? "block" : "hidden"}`}>
-          <FirstStep
-            nameInfo={nameInfo}
-            setNameInfo={setNameInfo}
-            setIsValid={setIsValid}
-            currentStep={currentStep}
+    <>
+      <div className="fixed top-0 left-0 z-50 bg-black/[0.5] w-screen h-screen flex justify-center items-center">
+        <div className="bg-white flex flex-col items-center justify-center gap-2 rounded-lg phone:w-[450px] phone:h-36">
+          <HashLoader
+            color="#00ADB5"
+            size={40}
+            aria-label="Loading Spinner"
+            data-testid="loader"
           />
+          <Loading message="Setting Up Your Task Command Center 🚀" />
         </div>
-        <div className={`${currentStep === 2 ? "block" : "hidden"}`}>
-          <SecondStep
-            additionalInfo={additionalInfo}
-            setAdditioNalInfo={setAdditionalInfo}
-            firstName={nameInfo.firstName}
-            setIsValid={setIsValid}
-            currentStep={currentStep}
-          />
-        </div>
-        <div className={`${currentStep === 3 ? "block" : "hidden"}`}>
-          <ThirdStep
-            accountInfo={accountInfo}
-            setAccountInfo={setAccountInfo}
-            setIsValid={setIsValid}
-            currentStep={currentStep}
-          />
-        </div>
-      </form>
-      <div className="w-full mt-8 flex justify-center items-center gap-10">
-        {currentStep !== 1 && (
-          <motion.button
-            type="button"
-            onClick={() => setCurrentStep((prev) => prev - 1)}
-            whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-            whileTap={{ scale: 0.9 }}
-            className="py-2 px-8 bg-Secondary text-LightSecondary"
-          >
-            Back
-          </motion.button>
-        )}
-        {currentStep !== 3 && (
-          <motion.button
-            type="button"
-            disabled={isValid ? false : true}
-            onClick={() => setCurrentStep((prev) => prev + 1)}
-            whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-            whileTap={{ scale: 0.9 }}
-            className={`py-2 px-8 ${
-              isValid ? "bg-LightPrimary" : "bg-Disabled"
-            } text-LightSecondary`}
-          >
-            Next
-          </motion.button>
-        )}
-        {currentStep === 3 && (
-          <motion.button
-            type="submit"
-            whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
-            whileTap={{ scale: 0.9 }}
-            className={`py-2 px-8 ${"bg-LightPrimary"} text-LightSecondary`}
-            onClick={() => {
-              mutation.mutate({
-                nameInfo: nameInfo,
-                additionalInfo: additionalInfo,
-                accountInfo: accountInfo,
-              });
-            }}
-          >
-            Submit
-          </motion.button>
-        )}
       </div>
-    </section>
+      <section className="bg-white rounded-2xl text-black p-3 relative phone:w-11/12">
+        <form>
+          <div className={`${currentStep === 1 ? "block" : "hidden"}`}>
+            <FirstStep
+              nameInfo={nameInfo}
+              setNameInfo={setNameInfo}
+              setIsValid={setIsValid}
+              currentStep={currentStep}
+            />
+          </div>
+          <div className={`${currentStep === 2 ? "block" : "hidden"}`}>
+            <SecondStep
+              additionalInfo={additionalInfo}
+              setAdditioNalInfo={setAdditionalInfo}
+              firstName={nameInfo.firstName}
+              setIsValid={setIsValid}
+              currentStep={currentStep}
+            />
+          </div>
+          <div className={`${currentStep === 3 ? "block" : "hidden"}`}>
+            <ThirdStep
+              accountInfo={accountInfo}
+              setAccountInfo={setAccountInfo}
+              setIsValid={setIsValid}
+              currentStep={currentStep}
+            />
+          </div>
+        </form>
+        <div className="w-full mt-8 flex justify-center items-center gap-10">
+          {currentStep !== 1 && (
+            <motion.button
+              type="button"
+              onClick={() => setCurrentStep((prev) => prev - 1)}
+              whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+              whileTap={{ scale: 0.9 }}
+              className="py-2 px-8 bg-Secondary text-LightSecondary"
+            >
+              Back
+            </motion.button>
+          )}
+          {currentStep !== 3 && (
+            <motion.button
+              type="button"
+              disabled={isValid ? false : true}
+              onClick={() => setCurrentStep((prev) => prev + 1)}
+              whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+              whileTap={{ scale: 0.9 }}
+              className={`py-2 px-8 ${
+                isValid ? "bg-LightPrimary" : "bg-Disabled"
+              } text-LightSecondary`}
+            >
+              Next
+            </motion.button>
+          )}
+          {currentStep === 3 && (
+            <motion.button
+              type="submit"
+              whileHover={{ scale: 1.1, transition: { duration: 0.2 } }}
+              whileTap={{ scale: 0.9 }}
+              className={`py-2 px-8 ${"bg-LightPrimary"} text-LightSecondary`}
+              onClick={() => {
+                mutation.mutate({
+                  nameInfo: nameInfo,
+                  additionalInfo: additionalInfo,
+                  accountInfo: accountInfo,
+                });
+              }}
+            >
+              Submit
+            </motion.button>
+          )}
+        </div>
+      </section>
+    </>
   );
 };
 
---
+++ components/ReusableComponents/Loading/Loading.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import HashLoader from "react-spinners/HashLoader";
+
+interface props {
+    message: string
+}
+
+const Loading = ({message}:props) => {
+  return (
+    <div className="fixed top-0 left-0 z-50 bg-black/[0.5] w-screen h-screen flex justify-center items-center">
+      <div className="bg-white flex flex-col items-center justify-center gap-2 rounded-lg phone:w-[450px] phone:h-36">
+        <HashLoader
+          color="#00ADB5"
+          size={40}
+          aria-label="Loading Spinner"
+          data-testid="loader"
+        />
+        <p className={`font-bold text-Primary`}>
+            {message}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
